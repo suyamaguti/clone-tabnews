dd697f74d77687627844b47ef52487d4cc474f79
Suellen Yamaguti <suellen.yamaguti@gmail.com>
2026-03-04 23:30:40 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-function Home(){
-    return <h1>Há um amigo que se apega mais que um irmão. Obrigada por ser minha irmã de coração.
-
+function Home() {
+  return (
+    <h1>
+      Há um amigo que se apega mais que um irmão. Obrigada por ser minha irmã de
+      coração.
     </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
